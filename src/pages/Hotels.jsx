eb7e93ebab9067b0d044c806b6a11af9f0994bb0
Harflexdev <tylermark200@gmail.com>
2024-10-28 12:45:52 +0000

    error fixed

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,9 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// const Hotels = () => {
-//   return (
-//     <div>lorem400</div>
-//   )
-// }
+const Hotels = () => {
+  return (
+    <div>lorem400</div>
+  )
+}
 
-// export default Hotels
+export default Hotels
